ef1649fcbf8a8b6349b778a803beaeac5a0dafc3
khindaraj <111604705+khindaraj@users.noreply.github.com>
2024-06-10 14:27:38 -0700

    Update rtp.sh
    Update with hostname webpos.sasedemo.net

--- rtp.sh
+++ rtp.sh
@@ -2,7 +2,7 @@
 cd /root/scripts
 while true
 do
-/root/scripts/rtp.py --destination-ip 10.10.10.204
+/root/scripts/rtp.py --destination-host webpos.sasedemo.net
 s=$(shuf -i 1-30 -n 1)
 echo "`date` Sleeping for $s seconds"
 sleep $s
